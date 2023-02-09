8a6b722d0cab8174c89c2109018d126def600898
Ana <ana.smith@itglobers.com>
2023-02-09 17:53:56 -0500

    refactor: ListContextProvider content

--- react/components/BulletGroup/index.tsx
+++ react/components/BulletGroup/index.tsx
@@ -2,8 +2,8 @@
 import React, { PropsWithChildren } from 'react';
 import { useListContext, ListContextProvider } from 'vtex.list-context';
 import { BulletsSchema } from './BulletTypes';
-import { useDevice } from 'vtex.device-detector';
-import { useCssHandles } from 'vtex.css-handles';
+// import { useDevice } from 'vtex.device-detector';
+// import { useCssHandles } from 'vtex.css-handles';
 import { getBulletAsTSXList } from './modules/bulletsAsList';
 
 export interface BulletGroupProps {
@@ -14,7 +14,7 @@ const BulletGroup = ({
   bullets,
   children
 }: PropsWithChildren<BulletGroupProps>) => {
-  const { isMobile } = useDevice();
+  // const { isMobile } = useDevice();
   const { list } = useListContext() || [];
 
   console.log("bullets", bullets)
@@ -22,20 +22,13 @@ const BulletGroup = ({
   const bulletsContent = getBulletAsTSXList(bullets);
   const newListContextValue = list.concat(bulletsContent)
 
-  const CSS_HANDLES = ['bullet__container'];
-  const handles = useCssHandles(CSS_HANDLES);
+  // const CSS_HANDLES = ['bullet__container'];
+  // const handles = useCssHandles(CSS_HANDLES);
 
   return (
     <ListContextProvider list={newListContextValue}>
-    {
-      isMobile
-      ?
-      <div className={handles.bullet__container}>
-        {bulletsContent}
-      </div>
-      :
-      children
-    }
+    {children}
+
     </ListContextProvider>
   )
 }
