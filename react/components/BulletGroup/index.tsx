48302458c4c677167db71d50c86b81e610bcbe02
Ana <ana.smith@itglobers.com>
2023-02-17 11:14:53 -0500

    refactor: deleted comments

--- react/components/BulletGroup/index.tsx
+++ react/components/BulletGroup/index.tsx
@@ -1,9 +1,6 @@
-//@ts-ignore
 import React, { PropsWithChildren } from 'react';
 import { useListContext, ListContextProvider } from 'vtex.list-context';
 import { BulletsSchema } from './BulletTypes';
-// import { useDevice } from 'vtex.device-detector';
-// import { useCssHandles } from 'vtex.css-handles';
 import { getBulletAsTSXList } from './modules/bulletsAsList';
 
 export interface BulletGroupProps {
@@ -14,21 +11,14 @@ const BulletGroup = ({
   bullets,
   children
 }: PropsWithChildren<BulletGroupProps>) => {
-  // const { isMobile } = useDevice();
   const { list } = useListContext() || [];
-
-  console.log("bullets", bullets)
 
   const bulletsContent = getBulletAsTSXList(bullets);
   const newListContextValue = list.concat(bulletsContent)
 
-  // const CSS_HANDLES = ['bullet__container'];
-  // const handles = useCssHandles(CSS_HANDLES);
-
   return (
     <ListContextProvider list={newListContextValue}>
-    {children}
-
+      {children}
     </ListContextProvider>
   )
 }
